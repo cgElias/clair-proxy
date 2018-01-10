2948fc9e6317b07c5c1fef53041712e7f4e74676
Elias Tatros <elias.tatros@capgemini.com>
2018-01-10 15:23:49 +0100

    update

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def hello():
 def login():
     if request.method == 'POST':
         # content = request.get_json(force=True)
-        return 'some content'
+        return  request.get_json(force=True)
     else:
         return 'get url'
 
